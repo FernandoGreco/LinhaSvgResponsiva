d12b0e07eee144051e99e9bbda6e9f14b56b5b40
Fernando Greco <greco-fernando@hotmail.com>
2020-01-04 20:58:38 -0300

    valores buscados e formatados

--- main.js
+++ main.js
@@ -1,89 +1,77 @@
 function btnTxtArea(){
 
+ //pega o texto que esta no text area
  let texto = document.getElementById("exampleFormControlTextarea1").value;
+ 
+ let i = 0;
+ let vetorX1 = [];
+ let vetorY1 = [];
+
+ //preenche o vetor com valores de x1 (width) e y1 (heigh)
+ vetorX1 =  editTxt('x1',texto);
+ vetorY1 =  editTxt('y1',texto);
+
+ 
+ console.groupCollapsed('x1');
+  for (i = 0; i < vetorX1.length; i++) {
+    console.log("Array X1 - " +  vetorX1[i]);
+  }
+  console.groupEnd();
 
 
- let buscaLarguraTotal = texto.search("width");
+
+console.groupCollapsed('y2');
+  for (i = 0; i < vetorY1.length; i++) {
+    console.log("Array Y1 - " +  vetorY1[i]);
+  }
+console.groupEnd();
+
+  }
 
 
- //console.log(busca);
- var larguraTotal = texto.substr(buscaLarguraTotal+7,5);
 
+//busca recebe o valor a ser pesquisado no texto e encontra a posição que ele esta na string
+function editTxt(busca,texto){
 
-    var larguraSgvX1 = [];
-    var larguraSgvY1 = [];
-    var i = 0;
-    var x1=[];
-    var y1=[];
+  var largura = [];
+  var i = 0;
 
- //  var localX1 =  buscaTexto('x1',texto);
- //  console.log("local X1 achado no função busca Texto"+localX1);
+  //vetor onde será armazenado valores buscados no texto
+  var vetor=[];
 
- while (texto.search("x1") != -1) {
-   //busca parte do texto onde têm x1
-   // let buscaLarguraSvgX1 = texto.search("x1");
+ while (texto.search(busca) != -1) {
+
+   //busca parte do texto onde têm o valor enviado na busca
+    let local =  texto.search(busca);
   
-   let buscaLarguraSvgX1 = editTxt('x1',texto);
-   console.log("local X1 achado no função busca Texto"+buscaLarguraSvgX1);
-   
-   let buscaLarguraSvgY1 = texto.search("y1"); 
-
-
+    
    //Tira os aspas do texto
-     larguraSgvX1[i] = texto.substr(buscaLarguraSvgX1+4,7).replace('"','');
-     larguraSgvY1[i] = texto.substr(buscaLarguraSvgY1+4,7).replace('"', '');
-
-
-     //se caso a string tiver espaço executa o if
-     if(larguraSgvX1[i].search(" ") != -1 || larguraSgvY1[i].search(" ") != -1){
+   largura[i] = texto.substr(local+4,9).replace('"','');
+  
+    //se caso a string tiver espaço executa o if
+    if(largura[i].search(" ") != -1){
      
-    //encontra o local onde esta  espaço  
-     let localX1 = larguraSgvX1[i].search(" ");
-     let localY1 = larguraSgvY1[i].search(" "); 
-
-     //array valores x1 e y1
-     x1[i]=larguraSgvX1[i].slice(0,localX1);
-     y1[i]=larguraSgvY1[i].slice(0,localY1);
-     //corta a string após final do valor
-     console.log("aqui cortado X1 - " + larguraSgvX1[i].slice(0,localX1));
-     console.log("aqui cortado Y1 - " + larguraSgvY1[i].slice(0,localY1));
-     } 
+      //encontra o local onde esta  espaço  
+       let local = largura[i].search(" ");
+    
   
-   //corta a parte do texto que já achou o x1 o procura o próximo x1 
-    texto = texto.slice(buscaLarguraSvgX1+5, texto.length);
-    console.log("X1 - "+larguraSgvX1[i]);
-    console.log("Y1 - "+larguraSgvY1[i]);
-    i++;
-  }
+       //array valores 
+       vetor[i]=largura[i].slice(0,local);
 
-  for (i = 0; i < x1.length; i++) {
-    console.log("Array X1 - " +  x1[i]);
-    console.log("Array Y1 - " +  y1[i]);
-  }
-
-/*
- var txtCut = texto.slice(buscaLarguraSvg+4, texto.length);
- let buscaLarguraSvg2 = txtCut.search("x1");
- var larguraSgv2 = txtCut.substr(buscaLarguraSvg2+4,3);
-
-
- console.log(n[0]);
- //console.log(typeof(busca));
- console.log(larguraTotal);
- console.log(larguraSgv);
-console.log(larguraSgv2);*/
-}
-
-function editTxt(local,texto){
-
- while (texto.search(local) != -1) {
-
-   //busca parte do texto onde têm x1
-    return texto.search(local);
-  
-  
+       //corta a string após final do valor
+       largura[i].slice(0,local);
+     
+       } 
+    
+     //corta a parte do texto que já achou o x1 o procura o próximo x1 
+      texto = texto.slice(local+5, texto.length);
+      i++;
+    }
+    //retorna vetor com valores
+    return vetor;
+    
   
   }
 
-}
+
 
